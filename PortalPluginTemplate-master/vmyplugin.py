20acdf0a253c58cddf056556c509b8da29614fa3
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-17 11:34:17 +0200

    fixed urls bug

--- PortalPluginTemplate-master/vmyplugin.py
+++ PortalPluginTemplate-master/vmyplugin.py
@@ -7,7 +7,7 @@ from .models import MyPluginModel
 from django.shortcuts import get_object_or_404
 from .forms import MyPluginForm
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from portal.vidispine.iuser import UserHelper
 from portal.vidispine.igeneral import performVSAPICall
 log = logging.getLogger(__name__)
