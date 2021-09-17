c1722e5f9d735491ec11cff4679c9f101ae66f6a
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-17 13:07:16 +0200

    test3

--- PortalPluginTemplate-master/vmyplugin.py
+++ PortalPluginTemplate-master/vmyplugin.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 # from .models import MyPluginModel
 # from django.shortcuts import get_object_or_404
 # from .forms import MyPluginForm
-# from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect
 # from django.urls import reverse
 # from portal.vidispine.iuser import UserHelper
 # from portal.vidispine.igeneral import performVSAPICall
