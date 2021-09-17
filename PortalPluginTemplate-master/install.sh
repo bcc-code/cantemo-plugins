57f3d52d69699b8daa71c977539ce346896362a8
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-17 13:58:51 +0200

    final cuttdown

--- PortalPluginTemplate-master/install.sh
+++ PortalPluginTemplate-master/install.sh
@@ -13,6 +13,6 @@ sudo mkdir -p $PORTAL_ROOT/portal/plugins/$PLUGIN_NAME
 sudo cp -r $DIR/* $PORTAL_ROOT/portal/plugins/$PLUGIN_NAME
 
 echo "Done."
-echo "Stop Portal: supervisorctl stop portal"
-echo "Sync the database: root@mediabox:/opt/cantemo/portal# python manage.py syncdb"
-echo "Start Portal: supervisorctl start portal"
+echo "Stop Portal: systemctl stop portal.target"
+echo "Sync the database: root@mediabox:   /opt/cantemo/portal   python manage.py syncdb"
+echo "Start Portal: systemctl start portal.target"
