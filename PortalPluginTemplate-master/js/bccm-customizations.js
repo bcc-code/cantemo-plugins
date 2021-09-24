70a3b18b0c01cadf400a66f6365c0bdcee05b9cd
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-24 14:42:04 +0200

    wip

--- PortalPluginTemplate-master/js/bccm-customizations.js
+++ PortalPluginTemplate-master/js/bccm-customizations.js
@@ -1,47 +1,11 @@
-/* 
-const pluginDiv = document.getElementById('theklasdksanjdkljasnd');
-var usergroup = pluginDiv.getAttribute(`data-user-group`)
-
-var oneOfManyButtons = document.createElement(`button`);
-oneOfManyButtons.classList = `some classes`
-
- */
-
 import config from '/sitemedia/js/bccm-customizations-config.js';
 //import config from '/bccm-customizations-config.js';
 
 console.log(`bccm-customizations.js executing`) 
-console.log('config', config);
-console.log('context', manualRulePluginContext);
 
+//creates button and add´s EventListener
 
-// function ruleButtonPlugin() {
-//     console.log(`RuleButtonPlugin initializing`)
-
-//     for (let group of manualRulePluginContext.user_groups) {
-//         console.log(group)
-//     }
-
-//      const button = document.getElementById('post-btn');
-//      button.addEventListener('click', async _ => {
-//          try {
-//              const response = await fetch('http://10.12.128.19/rulesengine3/start_process/?selected_objects=VX-6&', {
-//                  method: 'POST',
-//                  headers: {'Accept': 'application/json, text/javascript, */*; q=0.01'},
-//                  body: new URLSearchParams({
-//                      'csrfmiddlewaretoken': $("[name=csrfmiddlewaretoken]")[0].value,
-//                      'process_id': 'process_f50625b5-0c30-4364-b26e-4ec0f01db436',
-//                  })
-//              });
-//              console.log('Completed!', response);
-//          } catch(err) {
-//              console.error(`Error: ${err}`);
-//          }
-//      });
-//  }
-
-
-function addManualRuleButton2() {
+function addManualRuleButton() {
     for (var i = 0; i < FilterdUserGroups.length; i++) {
         for (var x = 0; x < FilterdUserGroups[i].buttons.length; x++) {
             let buttonConfig = FilterdUserGroups[i].buttons[x];
@@ -73,8 +37,6 @@ function addManualRuleButton2() {
     }
 }
 
-console.log(config.ruleButtonPlugin.userGroups[0].buttons[0].buttonColour)
-
 //filters out the user groups from the config file
 
 let FilterdUserGroups = config.ruleButtonPlugin.userGroups.filter(filterUserGroups);
@@ -87,11 +49,4 @@ function filterUserGroups(userGroup) {
     } 
 }
 
-console.log(FilterdUserGroups);
-
-
-
-
-
-// setTimeout(ruleButtonPlugin, 3000)
-setTimeout(addManualRuleButton2, 3000)
+setTimeout(addManualRuleButton, 2000)
