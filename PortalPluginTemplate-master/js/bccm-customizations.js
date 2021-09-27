4e2fb8838cf665dad3a8b0856772e742e0772890
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-27 11:06:36 +0200

    wip

--- PortalPluginTemplate-master/js/bccm-customizations.js
+++ PortalPluginTemplate-master/js/bccm-customizations.js
@@ -60,14 +60,20 @@ function PopupAnimateOut() {
 
 function popup(msg, isError) {
     if (isError) {
-        popup.style.color = "#ff0000"
-    }
+        popup = document.getElementById("myPopup");
+        popup.classList.add("show");
+        popup.classList.remove("remove");
+        setTimeout(PopupAnimateOut, 2000);
+        popup.innerHTML = msg;
+        popup.style.color = "ff0000"
+    } else {
     var popup = document.getElementById("myPopup");
-    popup.classList.add("show");
-    popup.classList.remove("remove");
-    setTimeout(PopupAnimateOut, 2000);
-    popup.innerHTML = msg;
-    popup.style.color = "#31ca3d"
+        popup.classList.add("show");
+        popup.classList.remove("remove");
+        setTimeout(PopupAnimateOut, 2000);
+        popup.innerHTML = msg;
+        popup.style.color = "#31ca3d"
+    }
 }
 
 function DisableButton() {
