6c91e82c1ff9173d56c3fb8beaf87e8899a5de01
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-27 11:31:04 +0200

    wip

--- PortalPluginTemplate-master/js/bccm-customizations.js
+++ PortalPluginTemplate-master/js/bccm-customizations.js
@@ -64,14 +64,14 @@ function popup(msg, isError) {
         popup.classList.remove("remove");
         setTimeout(PopupAnimateOut, 2000);
         popup.innerHTML = msg;
-        popup.style.color = "#e32222"
+        popup.style.color = "#c20c0c"
     } else {
         var popup = document.getElementById("myPopup");
         popup.classList.add("show");
         popup.classList.remove("remove");
         setTimeout(PopupAnimateOut, 2000);
         popup.innerHTML = msg;
-        popup.style.color = "#c20c0c"
+        popup.style.color = "#0da31a"
     }
 }
 
