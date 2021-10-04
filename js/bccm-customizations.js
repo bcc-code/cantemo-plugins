9d97c44302ba3f41583c13e3c751c6a9b6246bec
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-10-04 12:40:11 +0200

    test

--- js/bccm-customizations.js
+++ js/bccm-customizations.js
@@ -26,11 +26,11 @@ const http = new XMLHttpRequest()
 http.open("GET", '/API/v2/items/' + manualRulePluginContext.item_id + '/metadata/')
 http.send()
 
-http.onload = () => console.log(http.responseText)
-setTimeout(var group_name = http.responseText.group_name, 4900)
+http.onload = () => var group_name = http.responseText.group_name
+//console.log(http.responseText)
 
-setTimeout(console.log("group name:"), 5000)
-setTimeout(console.log(group_name), 5100)
+console.log("group name:")
+console.log(group_name)
 
 
 
