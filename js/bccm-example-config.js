2fcb6a3013d99e7e8d6b3f32769dc28c4f25330f
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-10-05 09:35:00 +0200

    test new if in addManualRuleButton and added metadata to config file and created new file for metadata constants

--- js/bccm-example-config.js
+++ js/bccm-example-config.js
@@ -83,7 +83,7 @@ export default {
                         process_id: `process_f50625b5-0c30-4364-b26e-4ec0f01db436`,
                         button_colour: colour.flamingo,
                         metadata: metadata.FILM
-                    
+                    }
                 ]
             }
         ]
