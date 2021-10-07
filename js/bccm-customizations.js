a1f84acd08f8f8494780797be5f4b64db6f52630
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-10-07 10:36:57 +0200

    new mutation observer function

--- js/bccm-customizations.js
+++ js/bccm-customizations.js
@@ -89,33 +89,26 @@ function PopupAnimateOUT() {
     popup.classList.add("remove");
 }
 
-// // Select the node that will be observed for mutations
-// const targetNode = document.getElementById('dropzone-wrapper');
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
 
-// // Options for the observer (which mutations to observe)
-// const config2 = { attributes: true, childList: true, subtree: true };
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
 
-// // Callback function to execute when mutations are observed
-// const callback = function(mutationsList, observer) {
-//     // Use traditional 'for loops' for IE 11
-//     for(const mutation of mutationsList) {
-//         if (mutation.type === 'childList') {
-//             console.log('A child node has been added or removed.');
-//         }
-//         else if (mutation.type === 'attributes') {
-//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
-//         }
-//     }
-// };
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
 
-// // Create an observer instance linked to the callback function
-// const observer = new MutationObserver(callback);
+waitForElm('#ManualRuleButtonDIV').then(elm => console.log(elm.textContent));
 
-// // Start observing the target node for configured mutations
-// observer.observe(targetNode, config2);
-
-// // Later, you can stop observing
-// observer.disconnect();
-
-
-setTimeout(addManualRuleButton, 3000);
+//setTimeout(addManualRuleButton, 3000);
