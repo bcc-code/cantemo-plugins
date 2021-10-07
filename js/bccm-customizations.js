53b422894c7ad37f80db7edbd1a3d92aa33711ca
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-10-07 09:53:46 +0200

    changed css a bit

--- js/bccm-customizations.js
+++ js/bccm-customizations.js
@@ -13,7 +13,9 @@ fetch('/API/v2/items/' + manualRulePluginContext.item_id + '/metadata/').then(re
 function addManualRuleButton() {
     for (var i = 0; i < FilterdUserGroup.length; i++) {
         for (var x = 0; x < FilterdUserGroup[i].buttons.length; x++) {
-            if(FilterdUserGroup[i].buttons[x].metadata == metadata_obj.group_name){
+
+            if(FilterdUserGroup[i].buttons[x].metadata == metadata_obj.group_name || FilterdUserGroup[i].buttons[x].metadata == ""){
+
                 let buttonConfig = FilterdUserGroup[i].buttons[x];
                 let ManualRuleButton = document.createElement("button");
                 ManualRuleButton.innerHTML = buttonConfig.label;
@@ -39,8 +41,7 @@ function addManualRuleButton() {
                     if (!response.ok) {
                         console.error('Didnt succeed!', response);
                         PopupAnimateIN("1 item already being processed", true)
-                    }
-                    else {
+                    } else {
                         console.log('Completed!', response);
                         PopupAnimateIN("1 item queued for processing!", false)
                     }
@@ -87,5 +88,34 @@ function PopupAnimateOUT() {
     popup.classList.remove("show");
     popup.classList.add("remove");
 }
+
+// // Select the node that will be observed for mutations
+// const targetNode = document.getElementById('dropzone-wrapper');
+
+// // Options for the observer (which mutations to observe)
+// const config2 = { attributes: true, childList: true, subtree: true };
+
+// // Callback function to execute when mutations are observed
+// const callback = function(mutationsList, observer) {
+//     // Use traditional 'for loops' for IE 11
+//     for(const mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             console.log('A child node has been added or removed.');
+//         }
+//         else if (mutation.type === 'attributes') {
+//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
+//         }
+//     }
+// };
+
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(callback);
+
+// // Start observing the target node for configured mutations
+// observer.observe(targetNode, config2);
+
+// // Later, you can stop observing
+// observer.disconnect();
+
 
 setTimeout(addManualRuleButton, 3000);
