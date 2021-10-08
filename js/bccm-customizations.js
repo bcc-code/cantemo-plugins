91c18ccff2606e946eaa2fbd760e5fe3149e4d7d
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-10-08 09:54:30 +0200

    revet changes

--- js/bccm-customizations.js
+++ js/bccm-customizations.js
@@ -20,7 +20,7 @@ function addManualRuleButton() {
                 let ManualRuleButton = document.createElement("button");
                 ManualRuleButton.innerHTML = buttonConfig.label;
                 ManualRuleButton.type = "button";
-                ManualRuleButton.setAttribute("class", buttonConfig.buttonColor);
+                ManualRuleButton.setAttribute("class", buttonConfig.button_colour);
                 document.getElementById("ManualRuleButtonDIV").appendChild(ManualRuleButton); 
 
                 ManualRuleButton.onclick = function() {
